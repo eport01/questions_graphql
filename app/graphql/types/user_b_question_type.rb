823bb3ef8438a03dd4505e8722e150c33b0e5e19
Emily Port <emilylport@gmail.com>
2023-03-18 14:19:42 -0600

    update b question on user b question query

--- app/graphql/types/user_b_question_type.rb
+++ app/graphql/types/user_b_question_type.rb
@@ -10,7 +10,7 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   
-    field :b_question, Types::TQuestionType, null: false do 
+    field :b_question, Types::BQuestionType, null: false do 
     end
     def b_question
       BQuestion.find(object.id)
