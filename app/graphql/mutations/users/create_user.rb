d9f66cf8c7c8d17b62cfa65b31d69059a67c0cc6
Emily Port <emilylport@gmail.com>
2023-03-18 12:38:06 -0600

    create user migration
    exit
    ;

--- app/graphql/mutations/users/create_user.rb
+++ app/graphql/mutations/users/create_user.rb
@@ -6,13 +6,17 @@ module Mutations
       argument :password_digest, String, required: true 
 
       field :user, Types::UserType, null:false 
+      # field :errors, [String], null: false 
 
       def resolve(name:, email:, password_digest:)
         user = User.new(name: name, email: email, password_digest: password_digest)
         if user.save 
           { user: user, errors: []}
         else 
-          raise GraphQL::ExecutionError, user.errors.full_messages.join(", ")
+          {
+            user: nil, 
+            errors: user.errors.full_messages
+          }       
         end
       end
 
