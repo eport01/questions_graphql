c3b903d2437c63263b72068740f147a264728f7e
Emily Port <emilylport@gmail.com>
2023-03-17 18:03:28 -0600

    update queries

--- app/graphql/types/b_question_type.rb
+++ app/graphql/types/b_question_type.rb
@@ -7,7 +7,7 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-    field :users, Types::UserType, null: true 
+    field :users, [Types::UserType] 
 
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -9,6 +9,7 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :t_questions, [Types::TQuestionType]
+    field :b_questions, [Types::BQuestionType]
 
     # field :tech, Types::TQuestionType, null: true
     # def tech 
