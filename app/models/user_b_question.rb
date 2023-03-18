763557f5cb3d6da6633cc5a89cb1660696f73065
Emily Port <emilylport@gmail.com>
2023-03-18 13:50:32 -0600

    able to update status

--- app/models/user_b_question.rb
+++ app/models/user_b_question.rb
@@ -1,5 +1,5 @@
 class UserBQuestion < ApplicationRecord
-  enum status: {active: 0, inactive: 1, favorited: 2}
+  # enum status: {active: 0, inactive: 1, favorited: 2}
   belongs_to :user
   belongs_to :b_question
 end
--- app/models/user_t_question.rb
+++ app/models/user_t_question.rb
@@ -1,5 +1,5 @@
 class UserTQuestion < ApplicationRecord
-  enum status: {active: 0, inactive: 1, favorited: 2}
+  # enum status: {active: 0, inactive: 1, favorited: 2}
   belongs_to :user
   belongs_to :t_question
 end
