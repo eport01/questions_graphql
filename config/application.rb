dcf9accd85f498a8d13f15b9d5640779d22ae58c
Emily Port <emilylport@gmail.com>
2023-03-23 13:53:37 -0600

    add session middleware

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module TaskMasterBe
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # This also configures session_options for use below
+    config.session_store :cookie_store, key: '_interslice_session'
+      
+    # Required for all session management (regardless of session_store)
+    config.middleware.use ActionDispatch::Cookies
+      
+    config.middleware.use config.session_store, config.session_options
+
   end
 end
