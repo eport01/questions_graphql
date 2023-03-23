0dcc5d894250be12d38117b3c3b342cfc2a60667
Emily Port <emilylport@gmail.com>
2023-03-23 16:46:43 -0600

    removed authentication from create user

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -2,25 +2,32 @@ module Mutations
 
     class CreateUser < Mutations::BaseMutation
 
-      class AuthProviderSignupData < Types::BaseInputObject
-        argument :credentials, Types::AuthProviderCredentialsInput, required: false
-      end
+      # class AuthProviderSignupData < Types::BaseInputObject
+      #   argument :credentials, Types::AuthProviderCredentialsInput, required: false
+      # end
 
       argument :name, String, required: true
-      argument :auth_provider, AuthProviderSignupData, required: false
-      # argument :email, String, required: true 
+      # argument :auth_provider, AuthProviderSignupData, required: false
+      argument :email, String, required: true 
+      argument :password, String, required: true 
+
       # type Types::UserType
       field :user, Types::UserType, null: true
-      field :token, String, null: true
+      # field :token, String, null: true
 
       # field :user, Types::UserType, null:false 
       field :errors, [String], null: false 
-      def resolve(name: nil, auth_provider: nil)
+      def resolve(name: nil, email: nil, password: nil)
         user = User.create!(
           name: name,
-          email: auth_provider&.[](:credentials)&.[](:email),
-          password: auth_provider&.[](:credentials)&.[](:password)
+          email: email,
+          password: password
         )
+        # user = User.create!(
+        #   name: name,
+        #   email: auth_provider&.[](:credentials)&.[](:email),
+        #   password: auth_provider&.[](:credentials)&.[](:password)
+        # )
         # require 'pry'; binding.pry
         # crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
         # token = crypt.encrypt_and_sign("user-id:#{ user.id }")
